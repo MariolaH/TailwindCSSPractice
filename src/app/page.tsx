9c7c34822bbcd6c8807e0ccbf74b7173f7ded909
Mariola Hullings <mariolahullings@gmail.com>
2025-02-06 14:06:17 -0500

    changed button color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-    <Link href="./sand"><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Click me</button></Link>
+    <Link href="./sand"><button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 py-4 rounded">Click me</button></Link>
     </div>
   );
 }
